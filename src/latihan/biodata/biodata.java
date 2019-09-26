67ae6efe2dbc6ec2667eb364ea46fe43ad63ea3f
Wildhevire <wildhevire404@gmail.com>
2019-09-26 00:23:15 +0000

    finished

---
+++ src/latihan/biodata/biodata.java
@@ -0,0 +1,22 @@
+package latihan.biodata;
+
+public class biodata {
+	/*
+    Nama        : Wildan Muhammad Fikri
+    Kelas       : IF1
+    NIM         : 10118024
+    Deskripsi   : Menampilkan Biodata
+     */
+
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------------------------");
+        System.out.println("NAMA\t\t: Wildan Muhammad Fikri");
+        System.out.println("ALAMAT\t\t: Jl. Sekeloa Utara, Coblong, Bandung");
+        System.out.println("TEMPAT LAHIR\t: SUBANG");
+        System.out.println("TANGGAL LAHIR\t: 14 Januari 2000");
+        System.out.println("---------------------------------------");
+        System.out.println("HOBI\t\t: Menggambar");
+        System.out.println("MAKES\t\t: Mie Sedap Goreng");
+        System.out.println("MIKES\t\t: Kopi");
+    }
+}
